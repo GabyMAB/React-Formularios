f16fa21f427e2e652586deb796a40ea9c022b386
gaby <gaby2001monse@outlook.com>
2024-02-03 18:18:48 -0600

    formularios en react

---
+++ src/components/Controlado.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+ 
+const Controlado = () =>{
+    const [todo, setTodo] = useState ({
+        title: 'todo#1',
+        description: 'descripcion #1',
+        state: 'pendiente',
+        priority: true,
+    })
+    const {title,description,state,priority} = todo;
+
+    const handleSubmit = (e) =>{
+        e.preventDefault(); 
+        console.log(title,description,state);
+    }
+    const handleChange = (e) =>{
+        const {name,type,checked,value} = e.target;
+       setTodo({
+            ...todo, [name]:type === "checkbox" ? checked: value,
+       })
+    };
+        return (
+            <form onSubmit={handleSubmit} >
+              <input 
+                    type="text" 
+                    placeholder="Ingresa Valores" 
+                    className="form-control mb-2"
+                    name="title"
+                    value={title}
+                    onChange={handleChange}>
+              </input>
+              <textarea
+                    className="form-control mb-2 " 
+                    placeholder="Ingresa Descripción"
+                    name="description"
+                    value={description}
+                    onChange={handleChange}
+              />
+              <div className="form-check mb-2">
+                <input 
+                    type="checkbox"
+                    name="priority"
+                    className="form-check-input"
+                    id="inputCheck"  
+                    checked={priority} 
+                    onChange={handleChange}             
+                />
+                
+                <label htmlFor="inputCheck">
+                    dar prioridad
+                </label>
+              </div>
+              <select className="form-select mb-2" 
+                    name="state" 
+                    value={state} 
+                    onChange={handleChange}>
+                        <option value="pendiente">Pendiente</option>  
+                        <option value="completado">Completado</option>  
+              </select>  
+              <button 
+                    type="submit" 
+                    className="btn btn-success">
+                    Enviar
+             </button>
+            </form>
+        );
+      };
+
+export default Controlado;
---
+++ src/components/NoControlado.jsx
@@ -0,0 +1,49 @@
+import { useState, useRef } from "react";
+
+ 
+const NoControlado = () =>{
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setError();
+        //console.log('me diste clic');
+        //console.log(form.current);
+        //console.log(new FormData(form.current));
+        //CAPTURAR DATOS
+        const data = new FormData(form.current);
+        //console.log(...data.entries());
+        const {title,description,state} = Object.fromEntries([...data.entries(),]);
+
+        console.log(title,description,state);
+
+        //VALIDAR DATOS
+        if(!title.trim() ||!description.trim() || !state.trim())
+         return setError("Llena todos los campos");
+    }
+    const form = useRef(null);
+    const [error, setError] = useState("");
+
+        return (
+            <form onSubmit={handleSubmit} ref={form}>
+              <input 
+              type="text" 
+              placeholder="Ingresa Valores" 
+              className="form-control mb-2"
+               name="title">
+              </input>
+              <textarea
+               className="form-control mb-2 " 
+              placeholder="Ingresa Descripción"
+              name="description"
+              />
+              <select className="form-select mb-2" name="state">
+                <option value="pendiente">Pendiente</option>  
+                <option value="completado">Completado</option>  
+              </select>  
+              <button type="submit" 
+              className="btn btn-primary">Enviar</button>
+              {error !== "" && error}
+            </form>
+        );
+      };
+
+export default NoControlado;
